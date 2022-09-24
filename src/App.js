8680f56429da21fd91eb11f75751303ca736a8e1
hridoy8299 <hridoy8299@gmail.com>
2022-09-24 10:59:58 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+    <Mobile></Mobile>
+
     </div>
   );
+}
+
+
+function Mobile(){
+  const [charge, setCharge]=useState(100);
+const MobileBattery= ()=> {
+
+  const newCharge = charge-10;
+  if(newCharge===-10){
+    return;
+  }
+  setCharge(newCharge);
+  
+}
+
+
+
+  return (
+    <div>
+      <h1>Mobile Name: Apple</h1>
+      <h3>Charge: {charge}</h3>
+      <button onClick={MobileBattery}>Change Battery Status</button>
+    </div>
+  )
 }
 
 export default App;
